7139cffc720bea3f3c48949b8f017ec6c3a4fef7
Irina Iskra <iskrairina.a@gmail.com>
2023-02-24 20:29:03 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", handleFormSubmit);
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const elements = {
+    email: event.currentTarget.email.value,
+    password: event.currentTarget.password.value,
+  };
+
+  if (elements.email === "" || elements.password === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+  console.log("Sent data:", elements);
+
+  event.currentTarget.reset();
+}
