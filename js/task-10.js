4864c569e5ef44bd6fcf829b957f0a216fe6432d
Irina Iskra <iskrairina.a@gmail.com>
2023-02-24 22:24:48 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,17 +3,47 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
 const controlsEl = document.querySelector("#controls");
 const createBtnEl = controlsEl.querySelector("[data-create]");
 const destroyBtnEl = controlsEl.querySelector("[data-destroy]");
 const amountEl = controlsEl.querySelector("input");
-console.log(controlsEl);
-console.log("createBtnEl ", createBtnEl);
-console.log("destroyBtnEl ", destroyBtnEl);
-console.log("amountEl ", amountEl);
+
+const boxesEl = document.querySelector("#boxes");
 
 createBtnEl.addEventListener("click", handleCreateBoxes);
 destroyBtnEl.addEventListener("click", handleDestroyBoxes);
 amountEl.addEventListener("change", handleGetAmount);
 
-function handleGetAmount(event) {}
+function createBoxes(amount) {
+  let height = 20;
+  let width = 20;
+  for (let i = 1; i <= amount; i += 1) {
+    height += 10;
+    width += 10;
+    boxesEl.insertAdjacentHTML(
+      "beforeend",
+      `<div style="width: ${height}px; height: ${width}px; background-color: ${getRandomHexColor()}"></div>`
+    );
+  }
+}
+
+function handleGetAmount(event) {
+  const value = event.currentTarget.value;
+
+  if (value === 0 || value > 100) {
+    event.currentTarget.value = "";
+    return alert("Enter a number from 1 to 100");
+  }
+  return value;
+}
+
+function handleCreateBoxes() {
+  const amount = amountEl.value;
+  createBoxes(amount);
+}
+
+function handleDestroyBoxes() {
+  boxesEl.innerHTML = "";
+  amountEl.value = "";
+}
