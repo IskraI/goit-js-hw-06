e45a3ae7bb6d7f1267dcefd2cd51064a14c98a46
Irina Iskra <iskrairina.a@gmail.com>
2023-02-24 20:51:06 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorChangeBtnEl = document.querySelector(".change-color");
+const colorSpanEl = document.querySelector("span.color");
+const bodyEl = document.querySelector("body");
+
+function handleChangeColorBody() {
+  const colorRandom = getRandomHexColor();
+  bodyEl.style.background = colorRandom;
+  colorSpanEl.textContent = colorRandom;
+}
+
+colorChangeBtnEl.addEventListener("click", handleChangeColorBody);
